0f654473f103ad2a9b76f86126bacc92493ce7bf
Rcatarata <69015151+Rcatarata@users.noreply.github.com>
2022-04-27 16:04:30 -0400

    Get and post
    implemented express for entry

--- pmr/src/App.js
+++ pmr/src/App.js
@@ -3,25 +3,35 @@ import Nav from  './components/Nav'
 import Hero from  './components/Hero'
 import Banner from './components/Banner'
 import Comments from './components/Comments'
+import {useState, useEffect} from 'react'
+import Axios from 'axios'
 
 function App() {
+  const [listOfEntries, setEntries] = useState([]);
+
+
+  useEffect(()=>{
+    Axios.get('http://localhost:3001/getEntry').then((response) => {
+      setEntries(response.data)
+    })
+  }, []);
+
   return (
     <div className="App">
       <Nav />
       <Banner />
       <Hero />
-      <Comments 
-        name = 'jeff'
-        date = 'today'
-      />
-      <Comments 
-        name = 'jeff'
-        date = 'today'
-      />
-      <Comments 
-        name = 'jeff'
-        date = 'today'
-      />
+      <div>
+        {listOfEntries.map((user) =>{
+          return (
+            <Comments 
+              name = {user.user}
+              entry = {user.entry}
+              date = {user._id}
+            />
+          )
+        })}
+      </div>
     </div>
   );
 }
--- pmr/src/components/Hero.js
+++ pmr/src/components/Hero.js
@@ -1,22 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Axios from 'axios'
 
 export default function Hero() {
     let today = new Date();
     console.log(today.getDate());
     const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
     let name = month[today.getMonth()];
+    let todaysDate = `${name} ${today.getDate()}, ${today.getFullYear()}`
+    const [entry, setEntry] = useState('')
+
+
+    const createEntry = () =>{
+        Axios.post('http://localhost:3001/createEntry', {
+            user: todaysDate, 
+            entry: entry
+        }
+        ).then((response) => {
+            console.log('user Created')
+        })
+    }
 
 
     return (
         <div className='Hero'>
-            <h3>Today is {`${name} ${today.getDate()}, ${today.getFullYear()}`}</h3>
-            <form className='hero--form'>
+            <h3>Today is {todaysDate}</h3>
+            <form action='/' className='hero--form'>
                 <label htmlFor='entry' >Todays Entry</label>
                 <br></br>
-                <textarea className='hero--textbox' name='entry' id='entry'/>
+                <textarea className='hero--textbox' name='entry' id='entry'
+                    onChange={(event)=> {setEntry(event.target.value)}}
+                />
                 <br></br>
-                <button className='submit--button'>Submit</button>
+                <button className='submit--button' onClick={createEntry}>Submit</button>
             </form>
+            
         </div>
     )
 }
