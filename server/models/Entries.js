a843c132fac5a08f0c8ae6f1d352c071084d5c57
Rcatarata <69015151+Rcatarata@users.noreply.github.com>
2022-04-26 22:19:06 -0400

    creating backend
    backend

---
+++ server/models/Entries.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const EntrySchema = new mongoose.Schema({
+    user: {
+        type: String,
+        require: true,
+    },
+    entry: {
+        type: String,
+        required: true
+    }
+});
+
+const EntryModel = mongoose.model('entries', EntrySchema)
+module.exports = EntryModel;
